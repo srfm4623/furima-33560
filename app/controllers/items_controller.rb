3fab374a26808a0280dc984e0c644d6c7a76d4f1
srfm4623 <srfm4623@gmail.com>
2021-01-28 17:14:25 +0900

    商品情報編集機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,8 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :same_params, only: [:show, :edit]
+  before_action :seller_identification, only: :edit
+
 
   def index
     @items = Item.all.order("created_at DESC")
@@ -19,7 +22,18 @@ class ItemsController < ApplicationController
   end
 
   def show
+  end
+
+  def edit
+  end
+
+  def update
     @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path
+    else
+      render :edit
+    end
   end
 
   private
@@ -28,4 +42,15 @@ class ItemsController < ApplicationController
     params.require(:item).permit(:image, :title, :text, :category_id, :condition_id, :shipping_cost_id, :prefecture_id,
                                  :arrival_date_id, :price).merge(user_id: current_user.id)
   end
+
+  def same_params
+    @item = Item.find(params[:id])
+  end
+
+  def seller_identification
+    unless current_user == @item.user
+      redirect_to root_path
+    end
+  end
+
 end
