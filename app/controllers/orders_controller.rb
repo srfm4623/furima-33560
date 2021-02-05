bee6bc28e317e1c51a094ab012ba8bd2bc8808b0
srfm4623 <srfm4623@gmail.com>
2021-02-05 18:34:10 +0900

    決済機能の実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,18 +1,20 @@
 class OrdersController < ApplicationController
+  before_action :same_params, only: [:index, :create]
   before_action :seller_identification, only: :index
 
-
   def index
-    @item = Item.find(params[:item_id])
+    # @item = Item.find(params[:item_id])
     @ordering_party = OrderingParty.new
   end
 
   def create 
-    @item = Item.find(params[:item_id])
+    # @item = Item.find(params[:item_id])
+    # binding.pry
     @ordering_party = OrderingParty.new(ordering_party_params)
     if @ordering_party.valid?
+      pay_item
       @ordering_party.save
-      redirect_to root_path
+      return redirect_to root_path
     else
       render :index
     end
@@ -20,15 +22,28 @@ class OrdersController < ApplicationController
 
   private
 
-  def seller_identification
+  def same_params
     @item = Item.find(params[:item_id])
-    redirect_to root_path unless user_signed_in? && current_user != @item.user
+  end
 
+
+  def seller_identification
+    # @item = Item.find(params[:item_id])
+    redirect_to root_path unless user_signed_in? && current_user != @item.user
   end
 
   def ordering_party_params
-    item = Item.find(params[:item_id])
-    params.require(:ordering_party).permit(:post_number, :prefecture_id, :city, :house_number, :building_name, :phone_number).merge(user_id: current_user.id, item_id: item.id)
+    @item = Item.find(params[:item_id])
+    params.require(:ordering_party).permit(:post_number, :prefecture_id, :city, :house_number, :building_name, :phone_number).merge(user_id: current_user.id, item_id: @item.id, price: @item.price, token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]  # 自身のPAY.JPテスト秘密鍵を記述しましょう
+    Payjp::Charge.create(
+      amount: ordering_party_params[:price], # 商品の値段
+      card: ordering_party_params[:token],   # カードトークン
+      currency: 'jpy'                 # 通貨の種類（日本円）
+    )
   end
 
 end
--- app/models/ordering_party.rb
+++ app/models/ordering_party.rb
@@ -1,9 +1,9 @@
 class OrderingParty
   include ActiveModel::Model
-  attr_accessor :user_id, :item_id, :post_number, :prefecture_id, :city, :house_number, :building_name, :phone_number, :order_id, :token
+  attr_accessor :user_id, :item_id, :post_number, :prefecture_id, :city, :house_number, :building_name, :phone_number, :order_id, :price, :token
 
   with_options presence: true do
-    # validates :token
+    validates :token
     validates :user_id
     validates :item_id
     validates :post_number,   format: { with: /\A\d{3}[-]\d{4}\z/, message: 'input correctly' }
@@ -14,6 +14,7 @@ class OrderingParty
   end
 
   def save
+    # item = Item.create(price: price)
     order = Order.create(user_id: user_id, item_id: item_id)
     Address.create(post_number: post_number, prefecture_id: prefecture_id, city: city, house_number: house_number, building_name: building_name, phone_number: phone_number, order_id: order.id)
   end
